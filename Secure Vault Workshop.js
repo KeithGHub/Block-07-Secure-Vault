90dda315f229044fc2614f26afcf6ee0c7b0d79c
OguKeith123 <keithogu1@gmail.com>
2024-11-19 09:11:11 -0500

    initial commit

---
+++ Secure Vault Workshop.js
@@ -0,0 +1,13 @@
+/*
+PSEUDOCODE: STEPS TO SECURE THE VAULT
+1. This vault will require a 3-digiit combination to access
+2. A combination will be developed with the use of mathematics
+*/
+
+alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
+);
+
+let sum1 = 10 + 9;
+let sum2 = sum1 * 8;
+let sum3 = sum2 / 2;
